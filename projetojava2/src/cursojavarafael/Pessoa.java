0c633d13ce982204777f546049aa60b354547f6f
Calangus <Calangus@DESKTOP-5GF6143>
2025-04-15 16:06:55 -0300

    Primeiro Commit

--- projetojava2/src/cursojavarafael/Pessoa.java
+++ projetojava2/src/cursojavarafael/Pessoa.java
@@ -1,5 +1,29 @@
 package cursojavarafael;
 
 public class Pessoa {
+	
+	
+	private String nome;
+	
+	private String Pessoa;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getPessoa() {
+		return Pessoa;
+	}
+
+	public void setPessoa(String pessoa) {
+		Pessoa = pessoa;
+	}
+	
+	
+	
 
 }
